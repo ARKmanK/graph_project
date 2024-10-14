445492f1a6c0d6b28293a3eb952b11fe9627b34b
ARKman <artyom.korepanov.1@gmail.com>
2024-10-14 21:10:05 +0800

    eigh'th commit

--- main.py
+++ main.py
@@ -46,15 +46,20 @@ class Interface(QtWidgets.QWidget):
         self.ui.close_button_2.clicked.connect(self.close_all_menus)
         self.ui.close_button_3.clicked.connect(self.close_all_menus)
         self.ui.close_button_7.clicked.connect(self.close_all_menus)
+        self.ui.close_button_8.clicked.connect(self.close_all_menus)
         self.ui.screenshot_button.clicked.connect(self.screenshot)
         self.ui.pushButton.clicked.connect(self.add_node)
         self.ui.pushButton_2.clicked.connect(self.open_connection_menu)
-        self.ui.pushButton_3.clicked.connect(self.open_change_color_menu)
-        self.ui.pushButton_4.clicked.connect(self.open_change_name_menu)
-        self.ui.pushButton_5.clicked.connect(self.clear_graph)        
+        self.ui.pushButton_3.clicked.connect(self.open_add_distance_menu)
+        self.ui.pushButton_4.clicked.connect(self.open_change_color_menu)
+        self.ui.pushButton_5.clicked.connect(self.open_change_name_menu)
+        
+        self.ui.pushButton_9.clicked.connect(self.clear_graph)        
         self.ui.pushButton_6.clicked.connect(self.change_node_name)
         self.ui.pushButton_7.clicked.connect(self.add_connection)
+        self.ui.pushButton_16.clicked.connect(self.add_distance)
         self.ui.pushButton_15.clicked.connect(self.change_color_original)
+        
         self.ui.listWidget.itemSelectionChanged.connect(self.change_color_yellow)
         self.ui.listWidget_2.itemSelectionChanged.connect(self.change_color_green)
 
@@ -64,34 +69,7 @@ class Interface(QtWidgets.QWidget):
         self.node_count = 0 
         #--------------------------------------------------------#
 
-
-    def minimize(self):
-        self.setWindowState(Qt.WindowState.WindowMinimized)
-
-    def screenshot(self):
-        pass
-
-    def notification(self, option=None):
-        options_dict = {
-            "screenshot": "Скриншот был добавлен в буфер обмена",
-            "wipe": "Рабочая область очищена"
-        }
-        for key, val in options_dict.items():
-            if option == key:
-                self.ui.notification_frame.raise_()
-                self.ui.plainTextEdit.setPlainText(val)                
-                QTimer.singleShot(3000, self.ui.notification_frame.lower)
-
-    def close_all_menus(self):
-        self.ui.text_edit_menu.lower()
-        self.ui.connection_menu.lower()
-        self.ui.change_color_menu.lower()
-
-    def add_node(self):
-        self.node_count += 1
-        self.dot.node(str(self.node_count), f'Узел {self.node_count}')
-        self.render_and_show()
-
+    # Main methods--------------------------#
     def open_change_name_menu(self):
         self.close_all_menus()
         self.ui.text_edit_menu.raise_()
@@ -99,6 +77,35 @@ class Interface(QtWidgets.QWidget):
         self.ui.comboBox_4.clear()
         for node_name in node_names:
             self.ui.comboBox_4.addItem(node_name)
+
+    def open_connection_menu(self):
+        self.close_all_menus()
+        self.ui.connection_menu.raise_()
+        node_names = self.get_nodes_names()
+        self.ui.comboBox_2.clear()
+        self.ui.comboBox_3.clear()
+        for node_name in node_names:
+            self.ui.comboBox_2.addItem(node_name)
+            self.ui.comboBox_3.addItem(node_name)
+
+    def open_change_color_menu(self):
+        self.close_all_menus()
+        self.ui.change_color_menu.raise_()
+        node_names = self.get_nodes_names()
+        self.ui.listWidget.clear()
+        self.ui.listWidget_2.clear()
+        self.ui.listWidget.addItems(node_names)
+        self.ui.listWidget_2.addItems(node_names)
+
+    def open_add_distance_menu(self):
+        self.close_all_menus()
+        self.ui.add_distance_menu.raise_()
+        node_names = self.get_nodes_names()
+        self.ui.comboBox_5.clear()
+        self.ui.comboBox_6.clear()
+        for node_name in node_names:
+            self.ui.comboBox_5.addItem(node_name)
+            self.ui.comboBox_6.addItem(node_name)
 
     def change_node_name(self):
         old_node_name = self.ui.comboBox_4.currentText()
@@ -110,6 +117,85 @@ class Interface(QtWidgets.QWidget):
                 self.dot.body[i] = new_line
         self.render_and_show()
 
+    def add_connection(self):
+        node_1 = self.ui.comboBox_2.currentText()
+        node_2 = self.ui.comboBox_3.currentText() 
+        self.dot.edge(node_1, node_2)
+        new_body = []
+        for line in self.dot.body:
+            if not (re.search(r'label="{}"'.format(node_1), line) or
+                    re.search(r'label="{}"'.format(node_2), line)):
+                new_body.append(line)
+        self.dot.body = new_body
+        self.dot.body = sorted(self.dot.body, key=lambda x: (int(re.search(r'\d+', x).group(0)) if re.search(r'\d+', x) else float('inf'), x))          
+        self.render_and_show()
+
+    def change_color_yellow(self):
+        selected_item = self.ui.listWidget.selectedItems()
+        if selected_item:
+            node_name = selected_item[0].text() 
+            self.delete_node(node_name)
+
+            # Проверка для дубликат
+            for line in self.dot.body:
+                if re.search(r'^\s*"{}" \[fillcolor=green style=filled\]\s*$'.format(node_name), line):
+                    self.dot.body.remove(line)
+                    break
+            
+            
+
+            self.dot.node(node_name, style="filled", fillcolor="yellow")
+            self.dot.body.sort(key=lambda x: x.strip().split(' ')[1].strip('"'))
+            self.remove_duplicate_nodes()
+            self.render_and_show()
+
+
+    def change_color_green(self):
+        selected_item = self.ui.listWidget_2.selectedItems()
+        if selected_item:
+            node_name = selected_item[0].text()
+            self.delete_node(node_name)
+
+            for line in self.dot.body:
+                if re.search(r'^\s*"{}" \[fillcolor=yellow style=filled\]\s*$'.format(node_name), line):
+                    self.dot.body.remove(line)
+                    break
+            
+            self.dot.node(node_name, style="filled", fillcolor="green")
+            self.dot.body.sort(key=lambda x: x.strip().split(' ')[1].strip('"'))
+            self.remove_duplicate_nodes()
+            self.render_and_show()
+
+    def change_color_original(self):
+        node_names = self.get_nodes_names()        
+        for name in node_names:
+            self.dot.node(name, style="filled", fillcolor="white")
+        self.render_and_show()
+
+    def add_distance(self):
+        node_1 = self.ui.comboBox_5.currentText()
+        node_2 = self.ui.comboBox_6.currentText() 
+        length = self.ui.textEdit_2.toPlainText()
+
+        for line in self.dot.body:
+            if re.search(r'"{}" -> "{}" \[.*\]'.format(node_1, node_2), line):
+                self.dot.body.remove(line)
+                break
+
+        self.delete_node(node_1)
+        self.delete_node(node_2)
+        self.dot.edge(node_1, node_2, weight=length, label=length, color="black")
+        self.render_and_show()
+
+    def add_node(self):        
+        self.node_count += 1
+        self.dot.node(str(self.node_count), f'Узел {self.node_count}')
+        self.render_and_show()
+        self.update_all_menus()
+
+    #---------------------------------------#
+
+    # Support methods-----------------------#
     def get_nodes_names(self):
         body = self.dot.body
         node_names = []
@@ -137,32 +223,9 @@ class Interface(QtWidgets.QWidget):
                 if match:
                     node_name = match.group(1)
                     node_names.append(node_name)
-        node_names = sorted(list(set(node_names)))
-        print(node_names)
+        node_names = sorted(list(set(node_names)), key=lambda x: (int(re.search(r'\d+', x).group(0)) if re.search(r'\d+', x) else float('inf'), x))
+        #print(node_names)
         return node_names
-
-    def open_connection_menu(self):
-        self.close_all_menus()
-        self.ui.connection_menu.raise_()
-        node_names = self.get_nodes_names()
-        self.ui.comboBox_2.clear()
-        self.ui.comboBox_3.clear()
-        for node_name in node_names:
-            self.ui.comboBox_2.addItem(node_name)
-            self.ui.comboBox_3.addItem(node_name)
-
-    def add_connection(self):
-        node_1 = self.ui.comboBox_2.currentText()
-        node_2 = self.ui.comboBox_3.currentText() 
-        self.dot.edge(node_1, node_2)
-        new_body = []
-        for line in self.dot.body:
-            if not (re.search(r'label="{}"'.format(node_1), line) or
-                    re.search(r'label="{}"'.format(node_2), line)):
-                new_body.append(line)
-        self.dot.body = new_body
-        self.dot.body = sorted(self.dot.body, key=lambda x: (int(re.search(r'\d+', x).group(0)) if re.search(r'\d+', x) else float('inf'), x))          
-        self.render_and_show()
 
     def clear_graph(self):
         self.dot.clear()
@@ -170,37 +233,6 @@ class Interface(QtWidgets.QWidget):
         self.ui.work_label_1.setPixmap(QPixmap())
         self.notification("wipe")
         self.close_all_menus()
-
-    def open_change_color_menu(self):
-        self.close_all_menus()
-        self.ui.change_color_menu.raise_()
-        node_names = self.get_nodes_names()
-        self.ui.listWidget.clear()
-        self.ui.listWidget_2.clear()
-        self.ui.listWidget.addItems(node_names)
-        self.ui.listWidget_2.addItems(node_names)
-
-    def change_color_yellow(self):
-        selected_item = self.ui.listWidget.selectedItems()
-        if selected_item:
-            node_name = selected_item[0].text()
-            self.dot.node(node_name, style="filled", fillcolor="yellow")
-            self.delete_node(node_name)
-            self.render_and_show()
-
-    def change_color_green(self):
-        selected_item = self.ui.listWidget_2.selectedItems()
-        if selected_item:
-            node_name = selected_item[0].text()
-            self.dot.node(node_name, style="filled", fillcolor="green")
-            self.delete_node(node_name)
-            self.render_and_show()
-
-    def change_color_original(self):
-        node_names = self.get_nodes_names()        
-        for name in node_names:
-            self.dot.node(name, style="filled", fillcolor="white")
-        self.render_and_show()
 
     def render_and_show(self):
         self.dot.render('graph', format='png')
@@ -210,9 +242,67 @@ class Interface(QtWidgets.QWidget):
         self.dot.body = [line for line in self.dot.body if f'label="{node_name}"' not in line]
         self.dot.body = sorted(self.dot.body, key=lambda x: x.split('"')[1] if '"' in x else x.split('[')[0])
 
+    def delete_connection(self, node_1=None, node_2=None):
+        self.dot.body = [line for line in self.dot.body if f'"{node_1}" -> "{node_2}"' not in line]
 
+    def update_all_menus(self):
+        node_names = self.get_nodes_names()
+        # Connection menu
+        self.ui.comboBox_2.clear()
+        self.ui.comboBox_3.clear()
+        for node_name in node_names:
+            self.ui.comboBox_2.addItem(node_name)
+            self.ui.comboBox_3.addItem(node_name)
+        # Color menu
+        self.ui.listWidget.clear()
+        self.ui.listWidget_2.clear()
+        self.ui.listWidget.addItems(node_names)
+        self.ui.listWidget_2.addItems(node_names)
+        # Distance menu
+        self.ui.comboBox_5.clear()
+        self.ui.comboBox_6.clear()
+        for node_name in node_names:
+            self.ui.comboBox_5.addItem(node_name)
+            self.ui.comboBox_6.addItem(node_name)
+        # Name menu
+        self.ui.comboBox_4.clear()
+        for node_name in node_names:
+            self.ui.comboBox_4.addItem(node_name)
 
+    def minimize(self):
+        self.setWindowState(Qt.WindowState.WindowMinimized)
 
+    def screenshot(self):
+        pass
+
+    def notification(self, option=None):
+        options_dict = {
+            "screenshot": "Скриншот был добавлен в буфер обмена",
+            "wipe": "Рабочая область очищена"
+        }
+        for key, val in options_dict.items():
+            if option == key:
+                self.ui.notification_frame.raise_()
+                self.ui.plainTextEdit.setPlainText(val)                
+                QTimer.singleShot(3000, self.ui.notification_frame.lower)
+
+    def close_all_menus(self):
+        self.ui.text_edit_menu.lower()
+        self.ui.connection_menu.lower()
+        self.ui.change_color_menu.lower()
+        self.ui.add_distance_menu.lower()
+
+    def remove_duplicate_nodes(self):
+        node_names = set()
+        new_body = []
+        for line in self.dot.body:
+            name = line.strip().split(' ')[1].strip('"')
+            if name not in node_names:
+                node_names.add(name)
+                new_body.append(line)
+        self.dot.body = new_body
+
+    #---------------------------------------#
 
 
 if __name__ == '__main__':
